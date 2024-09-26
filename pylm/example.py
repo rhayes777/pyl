149aacb89e8b3132c97aa2e720c7e5c01ba8915b
Richard <richard@rghsoftware.co.uk>
2024-09-26 11:45:22 +0100

    run the pylm test script on itself

---
+++ pylm/example.py
@@ -0,0 +1,13 @@
+
+def function(a, b):
+    '''
+    Add together two numbers.
+
+    Args:
+        a (int or float): The first number to be added.
+        b (int or float): The second number to be added.
+
+    Returns:
+        int or float: The sum of the two input numbers.
+    '''
+    return a + b
