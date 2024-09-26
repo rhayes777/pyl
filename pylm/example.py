05e20a6dccbe7284079da08fb2eaeb185468cfbd
Richard <richard@rghsoftware.co.uk>
2024-09-26 16:34:14 +0100

    regenerated example

--- pylm/example.py
+++ pylm/example.py
@@ -1,13 +1,20 @@
+from pylm import pylm
 
 def function(a, b):
-    '''
+    """
     Add together two numbers.
-
+    
     Args:
         a (int or float): The first number to be added.
         b (int or float): The second number to be added.
-
+        
     Returns:
         int or float: The sum of the two input numbers.
-    '''
+    """
     return a + b
+
+def other_function(c, d):
+    """
+    Multiple two numbers
+    """
+    return c * d
