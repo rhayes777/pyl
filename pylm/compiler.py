c26d82aecb55bf6b52f78b0bcdd3084d6d20e5bf
Richard <richard@rghsoftware.co.uk>
2024-09-26 10:49:49 +0100

    building compiler...

---
+++ pylm/compiler.py
@@ -0,0 +1,74 @@
+from abc import ABC
+from dataclasses import dataclass
+from typing import Optional
+
+from anthropic import AnthropicBedrock
+
+from pylm.module import Module
+from pylm.session import Function
+
+
+class Conversation(ABC):
+    def __init__(self):
+        self.messages = []
+
+    def clear(self):
+        self.messages = []
+
+
+class ClaudeConversation(Conversation):
+    def __init__(
+        self,
+        system: str,
+        model="anthropic.claude-3-haiku-20240307-v1:0",
+        max_tokens=4096,
+    ):
+        super().__init__()
+        self.system = system
+        self.client = AnthropicBedrock()
+        self.model = model
+        self.max_tokens = max_tokens
+
+    def __call__(self, text: str):
+        self.messages.append({"role": "user", "content": text})
+
+        message = self.client.messages.create(
+            system=self.system,
+            model=self.model,
+            messages=self.messages,
+            max_tokens=self.max_tokens,
+        )
+        text = message.content[0].text
+
+        self.messages.append({"role": "assistant", "content": text})
+        return text
+
+
+@dataclass
+class CompiledFunction:
+    source: Function
+    implementation: str
+
+
+class Compiler:
+    def __init__(
+        self,
+        module: Module,
+        conversation: Optional[Conversation] = None,
+    ):
+        self.module = module
+        self.conversation = conversation or ClaudeConversation(
+            system="""
+I will give you a function signature and a description of that function. 
+Return an implemented Python function with the same signature and description.
+            """
+        )
+
+    def compiled_functions(self):
+        for function in self.module.functions:
+            compiled = self._compile(function)
+            self.conversation.clear()
+            yield compiled
+
+    def _compile(self, function):
+        prompt = None
