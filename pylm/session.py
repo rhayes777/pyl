1f731dda7c35e211415e2d5bdc01b563a4f57c29
Richard <richard@rghsoftware.co.uk>
2024-10-06 07:38:43 +0100

    attempt to call from compiled

--- pylm/session.py
+++ pylm/session.py
@@ -1,20 +1,29 @@
+import importlib
 import inspect
 from pathlib import Path
 from typing import List
+
+from pylm.compiler import Compiler
 
 
 class Module:
     def __init__(
         self,
         path: Path,
+        module: str,
         functions: List["Function"] = None,
     ):
         self.path = path
+        self.module = module
         self.functions = functions or []
 
     @property
     def target_path(self):
         return self.path.with_stem(f"{self.path.stem}_pylm.py")
+
+    @property
+    def target_module(self):
+        return f"{self.module}_pylm"
 
 
 class Function:
@@ -34,8 +43,16 @@ class Function:
     def docs(self):
         return self.function.__doc__
 
+    def _call(self, *args, **kwargs):
+        module = importlib.import_module(self.module.target_module)
+        return getattr(module, self.name)(*args, **kwargs)
+
     def __call__(self, *args, **kwargs):
-        return self.function(*args, **kwargs)
+        try:
+            return self._call(*args, **kwargs)
+        except (ImportError, AttributeError):
+            Compiler(self.module).run()
+            return self._call(*args, **kwargs)
 
 
 class Session:
@@ -46,7 +63,7 @@ class Session:
         module_path = Path(function.__code__.co_filename)
 
         if module_path not in self.modules_by_path:
-            self.modules_by_path[module_path] = Module(module_path)
+            self.modules_by_path[module_path] = Module(module_path, function.__module__)
 
         module = self.modules_by_path[module_path]
         function_ = Function(function, module)
